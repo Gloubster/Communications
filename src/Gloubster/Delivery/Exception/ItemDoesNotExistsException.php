f5bc4a6e22a2dd80c5f89ecbeb6dae7b5a614542
Romain Neutron <imprec@gmail.com>
2012-06-29 15:45:00 +0200

    Add delivery exception

---
+++ src/Gloubster/Delivery/Exception/ItemDoesNotExistsException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Gloubster\Delivery\Exception;
+
+use \Gloubster\Exception\ExceptionInterface;
+
+class ItemDoesNotExistsException extends \RuntimeException implements ExceptionInterface
+{
+
+}
