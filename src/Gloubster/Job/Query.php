3292c9c25b34563e7fd298bf497c1a3aae80f31d
Romain Neutron <imprec@gmail.com>
2012-06-29 17:44:57 +0200

    Add Query

---
+++ src/Gloubster/Job/Query.php
@@ -0,0 +1,89 @@
+<?php
+
+/*
+ * This file is part of Gloubster.
+ *
+ * (c) Alchemy <info@alchemy.fr>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Gloubster\Job;
+
+use Gloubster\Delivery\Factory;
+use Gloubster\Exception\RuntimeException;
+
+class Query implements \Serializable
+{
+    protected $uuid;
+    protected $file;
+    protected $parameters;
+    protected $delivery;
+    protected $deliveryName;
+    protected $deliverySignature;
+
+    public function __construct($uuid, $file, $deliveryName, $deliverySignature, array $parameters = array())
+    {
+        $this->uuid = $uuid;
+        $this->file = $file;
+        $this->deliveryName = $deliveryName;
+        $this->deliverySignature = $deliverySignature;
+        $this->parameters = $parameters;
+    }
+
+    public function getUuid()
+    {
+        return $this->uuid;
+    }
+
+    public function getFile()
+    {
+        return $this->file;
+    }
+
+    public function getParameters()
+    {
+        return $this->parameters;
+    }
+
+    public function getDelivery(Factory $factory, $configuration)
+    {
+        if ( ! $this->delivery) {
+
+            $this->delivery = $factory->build($this->deliveryName, $configuration);
+
+            if ($this->delivery->getSignature() !== $this->deliverySignature) {
+                throw new RuntimeException('Wrong delivery configuration');
+            }
+        }
+
+        return $this->delivery;
+    }
+
+    public function serialize()
+    {
+        return json_encode(array(
+                'uuid'              => $this->uuid,
+                'file'              => $this->file,
+                'deliveryName'      => $this->deliveryName,
+                'deliverySignature' => $this->deliverySignature,
+                'parameters'        => $this->parameters,
+            ));
+    }
+
+    public function unserialize($serializedDatas)
+    {
+        $datas = json_decode($serializedDatas, true);
+
+        if (null === $datas) {
+            throw new RuntimeException('Data corrupted');
+        }
+
+        $this->uuid = $datas['uuid'];
+        $this->file = $datas['file'];
+        $this->deliveryName = $datas['deliveryName'];
+        $this->deliverySignature = $datas['deliverySignature'];
+        $this->parameters = $datas['parameters'];
+    }
+}
---
+++ tests/src/Gloubster/Job/QueryTest.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Gloubster\Job;
+
+class QueryTest extends \PHPUnit_Framework_TestCase
+{
+    protected $redisConfiguration;
+
+    public function setUp()
+    {
+        $this->redisConfiguration = array('host' => 'localhost', 'port' => 6379);
+    }
+
+    /**
+     * @covers Gloubster\Job\Query::getUuid
+     * @covers Gloubster\Job\Query::getFile
+     * @covers Gloubster\Job\Query::getParameters
+     * @covers Gloubster\Job\Query::getDelivery
+     * @covers Gloubster\Job\Query::__construct
+     * @todo Implement testGetUuid().
+     */
+    public function testGetters()
+    {
+        $uuid = 'randomId';
+        $file = 'http://file.jpg';
+        $deliveryName = 'RedisStore';
+        $deliverySignature = md5(json_encode(array_values($this->redisConfiguration)));
+        $parameters = array('width'  => 320, 'height' => 240);
+
+        $query = new Query($uuid, $file, $deliveryName, $deliverySignature, $parameters);
+
+        $this->assertEquals($uuid, $query->getUuid());
+        $this->assertEquals($file, $query->getFile());
+        $this->assertEquals($parameters, $query->getParameters());
+
+        $delivery = $query->getDelivery(new \Gloubster\Delivery\Factory, $this->redisConfiguration);
+
+        $this->assertEquals($deliveryName, $delivery->getName());
+        $this->assertEquals($deliverySignature, $delivery->getSignature());
+    }
+
+    /**
+     * @expectedException Gloubster\Exception\RuntimeException
+     */
+    public function testWrongSignature()
+    {
+        $query = new Query('randomId', 'http://file.jpg', 'RedisStore', 'randomwords');
+        $query->getDelivery(new \Gloubster\Delivery\Factory, $this->redisConfiguration);
+    }
+
+    /**
+     * @covers Gloubster\Job\Query::serialize
+     * @covers Gloubster\Job\Query::unserialize
+     */
+    public function testSerialize()
+    {
+        $query = new Query('randomId', 'http://file.jpg', 'RedisStore', md5(json_encode(array_values($this->redisConfiguration))));
+        $query2 = unserialize(serialize($query));
+        $this->assertEquals($query, $query2);
+    }
+
+    /**
+     * @covers Gloubster\Job\Query::unserialize
+     * @expectedException Gloubster\Exception\RuntimeException
+     */
+    public function testSerializingCorruption()
+    {
+        $query = $this->getMock('Gloubster\Job\Query', array('serialize'), array('randomId', 'http://file.jpg', 'RedisStore', 'randomwords'));
+
+        $query->expects($this->once())
+            ->method('serialize')
+            ->will(
+                $this->returnValue('prout !')
+        );
+
+        $query2 = unserialize(serialize($query));
+    }
+}
