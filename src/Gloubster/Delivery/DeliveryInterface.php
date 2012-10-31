24832dd8d0000f139d5f7b1d58b46c7d73e68a4c
Romain Neutron <imprec@gmail.com>
2012-10-31 15:40:04 +0100

    Update delivery interface

--- src/Gloubster/Delivery/DeliveryInterface.php
+++ src/Gloubster/Delivery/DeliveryInterface.php
@@ -11,75 +11,8 @@
 
 namespace Gloubster\Delivery;
 
-use Gloubster\Communication\Result;
-
-/**
- * Interface for deliveries.
- *
- * Delivery happens when a worker has finished his job. It returns the result
- * through the job delivery module
- */
-interface DeliveryInterface
+interface DeliveryInterface extends \Serializable
 {
-
-    /**
-     * Return the name of the delivery module. The name must be the classname
-     * inside the Gloubster\Delivery namespace
-     *
-     * @return string
-     */
-    public function getName();
-
-    /**
-     * Return a signature for the current delivery.
-     * The signature must be configuration dependant.
-     *
-     * This signature is used to confirm that a Worker uses the same
-     * configuration for the delivery module as the Client
-     *
-     * @return string
-     */
-    public function getSignature();
-
-    /**
-     * The deliver method is used by the Worker to deliver the output data.
-     *
-     * @param string $uuid The unique Id related to the job
-     * @param Result $result The result of the job
-     * @param string $binaryData The binaryData attached
-     *
-     * @throws Gloubster\Exception\RuntimeException On failure
-     */
-    public function deliver($uuid, Result $result, $binaryData);
-
-    /**
-     * The client use the retrieve method to retrieve the result of the worker
-     *
-     * @param string $uuid
-     *
-     * @throws Gloubster\Delivery\Exception\ItemDoesNotExistsException On failure
-     * @throws Gloubster\Exception\RuntimeException On failure
-     *
-     * @return Result
-     */
-    public function retrieve($uuid);
-
-    /**
-     * The client use the retrieveData method to retrieve the output of the
-     * transform
-     *
-     * @param type $uuid The unique Id related to the job
-     *
-     * @return string
-     */
-    public function retrieveData($uuid);
-
-    /**
-     * Build the delivery giving the configuration
-     *
-     * @param array $configuration
-     *
-     * @throws Gloubster\Exception\RuntimeException On failure
-     */
-    public static function build(array $configuration);
+    public function deliverBinary($data);
+    public function deliverFile($pathfile);
 }
