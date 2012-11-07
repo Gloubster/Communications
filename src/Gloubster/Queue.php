79c6bd314a050806d99768e0f132b2a21a846e3f
Romain Neutron <imprec@gmail.com>
2012-11-07 11:18:33 +0100

    Update log queue name

--- src/Gloubster/Queue.php
+++ src/Gloubster/Queue.php
@@ -4,7 +4,7 @@ namespace Gloubster;
 
 final class Queue
 {
-    const LOGS = 'phrasea.logs';
+    const LOGS = 'phrasea.subdefs.logs';
     const IMAGE_PROCESSING = 'phrasea.subdefs.image';
     const VIDEO_PROCESSING = 'phrasea.subdefs.video';
 }
