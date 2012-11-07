2476e73920c4fb73f073a0c121c8744a40af2c17
Romain Neutron <imprec@gmail.com>
2012-11-07 11:31:19 +0100

    Remove unused option, fix CS

--- src/Gloubster/RabbitMQFactory.php
+++ src/Gloubster/RabbitMQFactory.php
@@ -8,24 +8,24 @@ use PhpAmqpLib\Connection\AMQPSSLConnection;
 class RabbitMQFactory
 {
 
-    public static function createConnection(Configuration $conf, $connected = true)
+    public static function createConnection(Configuration $conf)
     {
         if (isset($conf['server']['ssl']) && $conf['server']['ssl']['enable']) {
             $connection = new AMQPSSLConnection(
-                    $conf['server']['host'],
-                    $conf['server']['port'],
-                    $conf['server']['user'],
-                    $conf['server']['password'],
-                    $conf['server']['vhost'],
-                    isset($conf['server']['ssl']['options']) ? $conf['server']['ssl']['options'] : array()
+                $conf['server']['host'],
+                $conf['server']['port'],
+                $conf['server']['user'],
+                $conf['server']['password'],
+                $conf['server']['vhost'],
+                isset($conf['server']['ssl']['options']) ? $conf['server']['ssl']['options'] : array()
             );
         } else {
             $connection = new AMQPConnection(
-                    $conf['server']['host'],
-                    $conf['server']['port'],
-                    $conf['server']['user'],
-                    $conf['server']['password'],
-                    $conf['server']['vhost']
+                $conf['server']['host'],
+                $conf['server']['port'],
+                $conf['server']['user'],
+                $conf['server']['password'],
+                $conf['server']['vhost']
             );
         }
 
