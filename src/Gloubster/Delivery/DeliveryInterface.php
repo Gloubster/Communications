848fd65b5e712452d666eacfda01eccffa50b620
Romain Neutron <imprec@gmail.com>
2012-11-08 18:05:03 +0100

    Add getName and getId methods to deliveryInterface

--- src/Gloubster/Delivery/DeliveryInterface.php
+++ src/Gloubster/Delivery/DeliveryInterface.php
@@ -13,6 +13,35 @@ namespace Gloubster\Delivery;
 
 interface DeliveryInterface extends \Serializable
 {
+
+    /**
+     * Returns delivery name
+     *
+     * @return string
+     */
+    public function getName();
+
+    /**
+     * Returns the unique Id to get the data back.
+     * This method can not be used before on of the delivery* methods
+     *
+     * @return string
+     */
+    public function getId();
+
+    /**
+     * Delivers a content based on a binary data
+     *
+     * @param string $data Binary data
+     * @return mixed A unique id to fetch the data back
+     */
     public function deliverBinary($data);
+
+    /**
+     * Delivers a content based on a path-file
+     *
+     * @param string $pathfile the path to the file
+     * @return mixed A unique id to fetch the data back
+     */
     public function deliverFile($pathfile);
 }
--- src/Gloubster/Delivery/FileSystem.php
+++ src/Gloubster/Delivery/FileSystem.php
@@ -22,21 +22,53 @@ class FileSystem implements DeliveryInterface
         $this->target = $target;
     }
 
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'filesystem';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getId()
+    {
+        return $this->target;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
     public function deliverBinary($data)
     {
         file_put_contents($this->target, $data);
+
+        return $this->target;
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function deliverFile($pathfile)
     {
         copy($pathfile, $this->target);
+
+        return $this->target;
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function serialize()
     {
         return json_encode(array('target' => $this->target));
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function unserialize($serialized)
     {
         $data = json_decode($serialized, true);
