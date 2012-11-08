1b71030ea662d40ab5a3d8701905abfcb2c34de8
Romain Neutron <imprec@gmail.com>
2012-11-09 00:08:41 +0100

    Add routing keys

--- src/Gloubster/RoutingKey.php
+++ src/Gloubster/RoutingKey.php
@@ -5,4 +5,6 @@ namespace Gloubster;
 final class RoutingKey
 {
     const LOGS = 'phrasea.logs';
+    const IMAGE_PROCESSING = 'phrasea.subdefs.image';
+    const VIDEO_PROCESSING = 'phrasea.subdefs.video';
 }
