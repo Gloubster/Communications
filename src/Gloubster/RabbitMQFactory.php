5e948219e3e6fcfda5ec76445d52f8b3c4197dc8
Romain Neutron <imprec@gmail.com>
2012-11-22 18:14:52 +0100

    Revert RabbitMQFactory to Gloubster Communications

---
+++ src/Gloubster/RabbitMQFactory.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Gloubster;
+
+use Gloubster\Configuration;
+use PhpAmqpLib\Connection\AMQPConnection;
+use PhpAmqpLib\Connection\AMQPSSLConnection;
+
+class RabbitMQFactory
+{
+
+    public static function createConnection(Configuration $conf)
+    {
+        if (isset($conf['server']['ssl']) && isset($conf['server']['ssl']['enable']) && true === $conf['server']['ssl']['enable']) {
+            $connection = new AMQPSSLConnection(
+                $conf['server']['host'],
+                $conf['server']['port'],
+                $conf['server']['user'],
+                $conf['server']['password'],
+                $conf['server']['vhost'],
+                isset($conf['server']['ssl']['options']) ? $conf['server']['ssl']['options'] : array()
+            );
+        } else {
+            $connection = new AMQPConnection(
+                $conf['server']['host'],
+                $conf['server']['port'],
+                $conf['server']['user'],
+                $conf['server']['password'],
+                $conf['server']['vhost']
+            );
+        }
+
+        register_shutdown_function(function(AMQPConnection $connection) {
+            try {
+                $connection->close();
+            } catch (\Exception $e) {
+
+            }
+        }, $connection);
+
+        return $connection;
+    }
+}
