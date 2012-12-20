2ce7298ae850bf04648ca59d67c1fc4c7e6a7aff
Romain Neutron <imprec@gmail.com>
2012-12-20 18:12:57 +0100

    Add tests on factory

--- src/Gloubster/Message/Factory.php
+++ src/Gloubster/Message/Factory.php
@@ -11,13 +11,28 @@
 
 namespace Gloubster\Message;
 
+use Gloubster\Exception\RuntimeException;
+
 class Factory
 {
+
     public static function fromJson($json)
     {
         $data = json_decode($json, true);
 
+        if (null === $data) {
+            throw new RuntimeException('Invalid Json data');
+        }
+
+        if (!isset($data['type'])) {
+            throw new RuntimeException('Message has no attribute type');
+        }
+
         $factory = implode('\\', array_slice(explode('\\', $data['type']), 0, -1)) . '\\Factory';
+
+        if (!class_exists($factory) || !method_exists($factory, 'fromJson')) {
+            throw new RuntimeException(sprintf('%s does not seem to be a valid factory', $factory));
+        }
 
         return $factory::fromJson($json);
     }
